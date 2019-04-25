00b36fcdb4974936aad2145c791b1c7ac90171ff
Francis Chen <fncis.chen@gmail.com>
2019-04-25 15:19:45 -0700

    edited yarn lock and package

---
+++ wellness-bet-sleep-frontend/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function Home(props) {
+    return 
+        <div className="Home"> 
+        "Welcome to our website."
+        </div>
+}
+
+export default Home
