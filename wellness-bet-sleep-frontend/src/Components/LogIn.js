150af8b2d6ce07ec458bc315faaff73921fd0f3a
Francis Chen <fncis.chen@gmail.com>
2019-04-26 14:21:58 -0700

    added messages for logging in and registration that respond and redirect you immediately to public users list

--- wellness-bet-sleep-frontend/src/Components/LogIn.js
+++ wellness-bet-sleep-frontend/src/Components/LogIn.js
@@ -8,12 +8,16 @@ class Login extends Component {
 
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            loggedIn: false, 
+            loginMessage: "Please log in."
         }
     }
 
     componentDidMount(){
-        
+        if(this.state.loggedIn == false){
+            this.setState({loginMessage: "Please log in."})
+        }
     }
 
 
@@ -24,7 +28,9 @@ class Login extends Component {
         this.setState({[e.target.name]: e.target.value});
     }
 
-    login() {
+    login = e => {
+
+        e.preventDefault();
 
         const user = {
             username: this.state.username,
@@ -32,11 +38,14 @@ class Login extends Component {
             email: this.state.email 
         }
 
-        axios.get("https://sleep-bet.herokuapp.com/login", user)
+        axios.post("https://sleep-bet.herokuapp.com/auth/login", user)
             .then(result => { 
+                this.setState({loggedIn: true, loginMessage: result.data.message })
                 console.log("Congratulations on logging on!");
                 return console.log(result)})
             .catch(error => console.log(error));
+
+        this.props.history.push("/users");
     }
 
     render() {
@@ -45,15 +54,19 @@ class Login extends Component {
 
         <div ClassName="login">
 
+           <div className="login-message">
+            {this.state.loginMessage}
+            </div>
+
         <h2>Login</h2>
 
         <form onSubmit={this.login}>
 
             <b>Name:</b>
-            <input name="username" type="text" onChange={() => this.handleChanges}></input>
+            <input name="username" type="text" onChange={(e) => this.handleChanges(e)}></input>
 
-            <b>Email:</b>
-            <input name="email" type="text" onChange={() => this.handleChanges}></input>
+            <b>Password:</b>
+            <input name="password" type="password" onChange={(e) => this.handleChanges(e)}></input>
             
             <button type="submit">Submit</button>
 
--- wellness-bet-sleep-frontend/src/Components/Register.js
+++ wellness-bet-sleep-frontend/src/Components/Register.js
@@ -9,13 +9,17 @@ class Register extends Component {
     this.state = {
         username: "",
         email: "",
-        password: ""
+        password: "",
+        registered: false,
+        welcomeMessage: "Please register before logging in."
     }
 
     }
 
     componentDidMount(){
-
+        if(this.state.registered == false){
+            this.setState({welcomeMessage: "Please register before logging in."})
+        }
     }
 
     handleChanges = e => {
@@ -24,7 +28,9 @@ class Register extends Component {
         this.setState({[e.target.name]: e.target.value});
     }
 
-    register() {
+    register = e => {
+
+        e.preventDefault();
         
         const user = {
             username: this.state.username,
@@ -32,12 +38,15 @@ class Register extends Component {
             email: this.state.email 
         }
 
-        axios.post("https://sleep-bet.herokuapp.com/register", user)
+        axios.post("https://sleep-bet.herokuapp.com/auth/register", user)
             .then(result => {
-                
+                console.log(result);
+                this.setState({registered: true, welcomeMessage: `Congratulations for registering, ${result.data.username}`})
                 console.log("Congratulations on registering!");
                 console.log(result)})
             .catch(error => console.log(error));
+
+            this.props.history.push("/users");
     }
 
     render() {
@@ -47,21 +56,26 @@ class Register extends Component {
 
             <h2>Register</h2>
 
+            <div className="registerMessage">
+            {this.state.welcomeMessage}
+            </div>
+
             <form onSubmit={this.register}>
                 <b>Name:</b>
-                <input name="username" type="text" onChange={() => this.handleChanges}></input>
+                <input name="username" type="text" onChange={(e) => this.handleChanges(e)}></input>
 
                 <b>Email:</b>
-                <input name="email" type="text" onChange={() => this.handleChanges}></input>
+                <input name="email" type="text" onChange={(e) => this.handleChanges(e)}></input>
 
                 <b>Password:</b>
-                <input name="password" type="password" onChange={() => this.handleChanges}></input>
+                <input name="password" type="password" onChange={(e) => this.handleChanges(e)}></input>
                 
                 <button type="submit">Submit</button>
             </form>
 
             </div>
         )
+        
     }
 
 }
