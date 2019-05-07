d3e06c3d28477dfdc87de3d67b17fd1f506d40de
Francis Chen <fncis.chen@gmail.com>
2019-05-07 16:20:09 -0700

    set up pseudo outline for sleep retrieval

--- wellness-bet-sleep-frontend/src/Components/FitBitOauth/FitbitOauth.js
+++ wellness-bet-sleep-frontend/src/Components/FitBitOauth/FitbitOauth.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import axios from "axios";
 
 var authorizationURL = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22942C&redirect_uri=http%3A%2F%2Fexample.com%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
 
@@ -11,10 +12,14 @@ export default class FitBitOAuth extends Component {
         this.state = {
 
         }
+
+
     }
 
     authorizeFitbitAccount = () => {
-        
+        // I need a button that creates a separate window that allows users to authenticate using Fitbit OAuth 
+        // After authentication, it should return the sleep data and place the user_id data from authentication to User components.
+        // The User Component has a GET Method that will then search for the sleep data for the given date range. 
     }
 }
 
