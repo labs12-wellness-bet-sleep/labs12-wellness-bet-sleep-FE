dc3338059bb31daa16cae674c974ad1190e1173a
Francis Chen <fncis.chen@gmail.com>
2019-05-08 15:40:12 -0700

    outlining main methods to retrieve data for group fitness dashboard

--- wellness-bet-sleep-frontend/src/Components/GroupDashboard/GroupDashboard.js
+++ wellness-bet-sleep-frontend/src/Components/GroupDashboard/GroupDashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import AggregatedSleepGraph from "./AggregatedSleepGraph.js";
 import TimeLeft from "./TimeLeft.js";
 import UserSleepStatus from "./UserSleepStatus.js";
@@ -8,17 +10,62 @@ export default class GroupDashboard extends Component {
 
     constructor(props){
         super(props)
+
+        this.state = {
+
+            groupID: "",
+
+            loggedInUsersAndData: {},
+            
+            fitbitUserID: "",
+            fitbitAccessToken: "",
+
+            currentUser: {},
+            currentUserSleep: "",
+            currentUserAggregatedSleep: "",
+
+            daysLeft: "",
+
+        }
+    }
+
+    componentDidMount() {
+        this.retrieveUserSleepData();
+        this.calculateTimeLeft();
+    }
+
+    getDateRange() {
+
+    }
+
+    calculateTimeLeft() {
+
+    }
+
+    // 
+
+    retrieveUserSleepData() {
+
+    }
+
+    amountOfSleepLeft() {
+
+    }
+
+    // go on fitBit to retrieve data?
+    logSleep() {
+
     }
 
     render(){
         return(
             <div className="GroupDashboard">
             
-            <UsersRanking/>
+            <UsersRanking users={this.state.loggedInUsersAndData}/>
             <TimeLeft/>
             <UserSleepStatus/>
 
-            <AggregatedSleepGraph/>
+            <AggregatedSleepGraph loggedInUsersAndData={this.state.loggedInUsersAndData}/>
             </div>
         );
     }
