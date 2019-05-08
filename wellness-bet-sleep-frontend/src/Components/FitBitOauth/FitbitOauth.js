be01eaa28b23642114d89f9fac6774f11601cdbd
Francis Chen <fncis.chen@gmail.com>
2019-05-08 00:55:22 -0700

    commented out fitbit o auth as this should go on the back end

--- wellness-bet-sleep-frontend/src/Components/FitBitOauth/FitbitOauth.js
+++ wellness-bet-sleep-frontend/src/Components/FitBitOauth/FitbitOauth.js
@@ -5,20 +5,20 @@ import passport from "passport";
 
 var authorizationURL = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22942C&redirect_uri=http%3A%2F%2Fexample.com%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
 
-var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+// var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
 
-// check callback oauth
-passport.use(new FitbitStrategy({
-    clientID:     FITBIT_CLIENT_ID,
-    clientSecret: FITBIT_CLIENT_SECRET,
-    callbackURL: "http://yourdormain:3000/auth/fitbit/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+// // check callback oauth
+// // passport.use(new FitbitStrategy({
+// //     clientID:     FITBIT_CLIENT_ID,
+// //     clientSecret: FITBIT_CLIENT_SECRET,
+// //     callbackURL: "http://yourdormain:3000/auth/fitbit/callback"
+// //   },
+// //   function(accessToken, refreshToken, profile, done) {
+// //     User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+// //       return done(err, user);
+// //     });
+// //   }
+// // ));
 
 
 // https://www.npmjs.com/package/passport-fitbit-oauth2
