84a38dccf1a34a3829184134b2cdf8173326eee2
Francis Chen <fncis.chen@gmail.com>
2019-05-08 14:22:39 -0700

    intialized Group Dashboard File

---
+++ wellness-bet-sleep-frontend/src/Components/GroupDashboard/GroupDashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class GroupDashboard extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="GroupDashboard">
+            
+            </div>
+        );
+    }
+}
