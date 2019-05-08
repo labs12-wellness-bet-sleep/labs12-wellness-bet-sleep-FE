5b6a9ae4dd9c1750b3528d5fabb7456499433407
Francis Chen <fncis.chen@gmail.com>
2019-05-08 14:34:58 -0700

    added main components for group sleep dashboard

--- wellness-bet-sleep-frontend/src/Components/GroupDashboard/GroupDashboard.js
+++ wellness-bet-sleep-frontend/src/Components/GroupDashboard/GroupDashboard.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import AggregatedSleepGraph from "./AggregatedSleepGraph.js";
+import TimeLeft from "./TimeLeft.js";
+import UserSleepStatus from "./UserSleepStatus.js";
+import UsersRanking from "./UsersRanking.js"
 
 export default class GroupDashboard extends Component {
+
     constructor(props){
         super(props)
     }
@@ -9,6 +14,11 @@ export default class GroupDashboard extends Component {
         return(
             <div className="GroupDashboard">
             
+            <UsersRanking/>
+            <TimeLeft/>
+            <UserSleepStatus/>
+
+            <AggregatedSleepGraph/>
             </div>
         );
     }
