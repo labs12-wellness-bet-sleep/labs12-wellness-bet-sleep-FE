564a36aeb089ee0f6fe2e6529e40f1ee66be7b89
Francis Chen <fncis.chen@gmail.com>
2019-05-10 15:10:32 -0700

    added sleep js file

--- wellness-bet-sleep-frontend/src/axios-sleep.js
+++ wellness-bet-sleep-frontend/src/axios-sleep.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL: "http://localhost:8080"});
+const instance = axios.create({baseURL: "http://localhost:5000"});
 
 
 export default instance;
