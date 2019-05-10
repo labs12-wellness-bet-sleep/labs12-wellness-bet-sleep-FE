88c839aaff8e69b307688f8f45e9ee8084632a43
Francis Chen <fncis.chen@gmail.com>
2019-05-09 18:36:06 -0700

    initialized redux and redux store, provider and router

--- wellness-bet-sleep-frontend/src/index.js
+++ wellness-bet-sleep-frontend/src/index.js
@@ -5,7 +5,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore( () => {}, composeEnhancers(applyMiddleware(thunk)));
+
+const app = (
+    <Provider store={store}>
+    <Router><App /></Router>
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
