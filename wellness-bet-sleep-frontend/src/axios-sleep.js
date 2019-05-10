9c4608e0d26bcffd10ed88aa693ba3299e9a9e1a
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-05-10 16:20:45 -0500

    fixed axios instance

--- wellness-bet-sleep-frontend/src/axios-sleep.js
+++ wellness-bet-sleep-frontend/src/axios-sleep.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL: "https://sleep-bet.herokuapp.com/"});
+const instance = axios.create({
+    baseURL: "https://sleep-bet.herokuapp.com/"
+});
 
 
 export default instance;
