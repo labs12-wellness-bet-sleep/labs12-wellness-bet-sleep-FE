4db0c3a8d358a019c05295a1f076e26856540039
Francis Chen <fncis.chen@gmail.com>
2019-05-09 23:25:26 -0700

    added user ranking component for leaderboard

---
+++ wellness-bet-sleep-frontend/src/Components/GroupDashboard/UserRanking.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Ranking = styled.div`
+    display: flex;
+    flex-direction: row; 
+    margin: 2%;
+    justify-content: space-between;
+`
+
+const ProfilePic = styled.div`
+    width: 25%;
+`
+
+export default class UserRanking extends Component {
+    
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+
+            <Ranking>
+                <div className="photo"><ProfilePic><img src={this.props.userSleepData.photo} width="45px"/></ProfilePic></div>
+                <div className="username">{this.props.userSleepData.username}</div>
+                <div className="sleepAmount">{this.props.userSleepData.amountOfSleep} hours</div>
+            </Ranking>
+        );
+    }
+}
