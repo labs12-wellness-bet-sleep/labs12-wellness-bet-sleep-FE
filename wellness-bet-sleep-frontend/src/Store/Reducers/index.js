fa4820edbba943fcab4148efb586baa273388c8d
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-05-15 14:40:19 -0500

    updates

--- wellness-bet-sleep-frontend/src/Store/Reducers/index.js
+++ wellness-bet-sleep-frontend/src/Store/Reducers/index.js
@@ -2,11 +2,5 @@ import authReducer from "./auth.js";
 import {combineReducers} from "redux";
 import groupReducer from './groups.js';
 
-<<<<<<< HEAD
-export default combineReducers({
-    auth: authReducer
-}); 
-=======
 export default combineReducers({auth: authReducer, groups: groupReducer }); 
 // groups: groupReducer
->>>>>>> 9d55577319eec988f8b7022b3d1b4240a3cea933
--- wellness-bet-sleep-frontend/src/index.js
+++ wellness-bet-sleep-frontend/src/index.js
@@ -4,14 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
-<<<<<<< HEAD
-import { MultiThemeProvider } from '@material-ui/core/styles/MuiThemeProvider';
-// import theme from './material-ui-theme'
-=======
 
-// ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-
->>>>>>> 9d55577319eec988f8b7022b3d1b4240a3cea933
 import {createStore, applyMiddleware, compose} from "redux";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
@@ -25,18 +18,11 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(RootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 const app = (
-<<<<<<< HEAD
     
     <Provider store={store}>
         <Router>
-        {/* <MultiThemeProvider > */}
             <App />
-        {/* </MultiThemeProvider> */}
         </Router>
-=======
-    <Provider store={store}>
-    <Router><App /></Router>
->>>>>>> 9d55577319eec988f8b7022b3d1b4240a3cea933
     </Provider>
 )
 
