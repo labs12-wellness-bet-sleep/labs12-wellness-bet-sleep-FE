387a79d196a28577a9664fd6e10a1ddd13c71ccb
Sem8 <mssemmi8@gmail.com>
2019-05-15 02:56:01 -0400

    delete group function in redux written but not tested

--- wellness-bet-sleep-frontend/src/Store/Reducers/groupReducer.js
+++ wellness-bet-sleep-frontend/src/Store/Reducers/groupReducer.js
@@ -1,39 +1,60 @@
-import axios from '../../axios-sleep';
-import { groupTypes } from '../Actions/actionTypes';
+import axios from "../../axios-sleep";
+import { groupTypes } from "../Actions/actionTypes";
 
 const initialState = {
-    addGroup: false,
-    groups: [],
-    groupId: null,
-    fetching: false,
-    errors: null
+  addGroup: false,
+  groups: [],
+  groupId: null,
+  fetching: false,
+  errors: null
 };
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case groupTypes.FETCH_GROUPS_START:
-        return {
-            ...state,
-            fetching: true,
-            errors: null
-        };
+  switch (action.type) {
+    case groupTypes.FETCH_GROUPS_START:
+      return {
+        ...state,
+        fetching: true,
+        errors: null
+      };
 
-        case groupTypes.FETCH_GROUPS_SUCCESS:
-        return {
-            ...state,
-            groups: action.payload,
-            fetching: false,
-            errors: null
-        };
-        case groupTypes.FETCH_GROUPS_FAILURE:
-        return {
-            ...state,
-            fetching: false,
-            errors: action.payload            
-        };
-        default: 
-        return state
-    }
-}
+    case groupTypes.FETCH_GROUPS_SUCCESS:
+      return {
+        ...state,
+        groups: action.payload,
+        fetching: false,
+        errors: null
+      };
+    case groupTypes.FETCH_GROUPS_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        errors: action.payload
+      };
 
+    case groupTypes.DELETE_GROUP_START:
+      return {
+        ...state,
+        fetching: true,
+        errors: null
+      };
 
+    case groupTypes.DELETE_GROUP_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        groups: state.groups.filter(group => group.id !== action.payload),
+        errors: null
+      };
+
+    case groupTypes.DELETE_GROUP_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
