8de7c5abe215c0f11bd2e9a87491cf9fa713da8e
fncischen <fncis.chen@gmail.com>
2019-05-21 09:44:05 -0700

    changed axios sleep url reference from localhost to heroku app
    changed axios sleep url reference from localhost to heroku app

--- wellness-bet-sleep-frontend/src/axios-sleep.js
+++ wellness-bet-sleep-frontend/src/axios-sleep.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL: "http://localhost:8080/"});
+const instance = axios.create({baseURL: "https://sleep-bet.herokuapp.com/"});
 
 export default instance;
